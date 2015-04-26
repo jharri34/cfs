bf7df7c5bf2bb26069dddac0e0abec0149b04718
jamallharris <jamallharris@absifs.com>
2015-04-26 11:20:53 -0500

    google maps and navbar complete

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,23 @@
  * Controller of the cfsApp
  */
 angular.module('cfs.controllers')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
+    .controller('MainCtrl', function ($scope) {
+        $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
-  });
+
+
+        var map;
+
+
+            var mapOptions = {
+                zoom: 8,
+                center: new google.maps.LatLng(-34.397, 150.644)
+            };
+            map = new google.maps.Map(document.getElementById('map-canvas'),
+                mapOptions);
+
+
+    });
--- app/scripts/directives/Navigation.js
+++ app/scripts/directives/Navigation.js
@@ -4,7 +4,19 @@ angular.module('cfs.directives').directive('navigation', function () {
     return {
         transclude: true,
         restrict: 'E',
-        templateUrl: "views/navigation.html"
+        templateUrl: "views/navigation.html",
+        controller: function ($scope, $element) {
+            console.log($scope, $element);
+            $(window).scroll(function () {
+                if ($(document).scrollTop() > 50) {
+                    $('nav').addClass('shrink');
+                    console.log("foo");
+                } else {
+                    $('nav').removeClass('shrink');
+                    console.log("bar");
+                }
+            });
+        }
     }
 
 });
