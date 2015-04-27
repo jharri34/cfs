80f1fbd3d5ca884f1f9c9c849d3d156158d882cf
jamallharris <jamallharris@absifs.com>
2015-04-27 11:43:55 -0500

    finished about staff section and fixed banner intro

--- app/scripts/directives/Navigation.js
+++ app/scripts/directives/Navigation.js
@@ -6,14 +6,11 @@ angular.module('cfs.directives').directive('navigation', function () {
         restrict: 'E',
         templateUrl: "views/navigation.html",
         controller: function ($scope, $element) {
-            console.log($scope, $element);
             $(window).scroll(function () {
                 if ($(document).scrollTop() > 50) {
                     $('nav').addClass('shrink');
-                    console.log("foo");
                 } else {
                     $('nav').removeClass('shrink');
-                    console.log("bar");
                 }
             });
         }
